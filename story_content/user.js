3f0d8f02d72195a3e24dd726a1cfa1becefe7b27
elsadefitaedu <tugaselsa13@gmail.com>
2026-03-10 06:09:00 +0700

    File Upload

---
+++ story_content/user.js
@@ -0,0 +1,199 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6evEygK6PMy":
+        Script1();
+        break;
+      case "6Sg7ElZW0aW":
+        Script2();
+        break;
+      case "5vkIWT3Cq8O":
+        Script3();
+        break;
+      case "6YTJYfvzsRE":
+        Script4();
+        break;
+      case "5jD5ZuujvGt":
+        Script5();
+        break;
+      case "6d7EpoK2qfe":
+        Script6();
+        break;
+      case "6ZdU5Q9zg74":
+        Script7();
+        break;
+      case "5fKnIoChXzW":
+        Script8();
+        break;
+      case "6a73d2Ivc4B":
+        Script9();
+        break;
+      case "6rNbcwLp4NW":
+        Script10();
+        break;
+      case "5gJlv2f9pg7":
+        Script11();
+        break;
+      case "5vRz8xyTjrq":
+        Script12();
+        break;
+      case "5miyyBO71ke":
+        Script13();
+        break;
+      case "5mAPendhwZW":
+        Script14();
+        break;
+      case "5i3gPyjtzWI":
+        Script15();
+        break;
+      case "6X35n76lURD":
+        Script16();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="SOUND.mp3";
+audio.load();
+audio.play();
+audio.volume = 0.4;
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="SOUND.mp3";
+audio.load();
+audio.play();
+audio.volume = 0.4;
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="SOUND.mp3";
+audio.load();
+audio.play();
+audio.volume = 0.4;
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="SOUND.mp3";
+audio.load();
+audio.play();
+audio.volume = 0.4;
+}
+
+function Script5()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="SOUND.mp3";
+audio.load();
+audio.play();
+audio.volume = 0.4;
+}
+
+function Script6()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="SOUND.mp3";
+audio.load();
+audio.play();
+audio.volume = 0.4;
+}
+
+function Script7()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="SOUND.mp3";
+audio.load();
+audio.play();
+audio.volume = 0.4;
+}
+
+function Script8()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="SOUND.mp3";
+audio.load();
+audio.play();
+audio.volume = 0.4;
+}
+
+function Script9()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="SOUND.mp3";
+audio.load();
+audio.play();
+audio.volume = 0.4;
+}
+
+function Script10()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="SOUND.mp3";
+audio.load();
+audio.play();
+audio.volume = 0.4;
+}
+
+function Script11()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="SOUND.mp3";
+audio.load();
+audio.play();
+audio.volume = 0.4;
+}
+
+function Script12()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="SOUND.mp3";
+audio.load();
+audio.play();
+audio.volume = 0.4;
+}
+
+function Script13()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="SOUND.mp3";
+audio.load();
+audio.play();
+audio.volume = 0.4;
+}
+
+function Script14()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="SOUND.mp3";
+audio.load();
+audio.play();
+audio.volume = 0.4;
+}
+
+function Script15()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="SOUND.mp3";
+audio.load();
+audio.play();
+audio.volume = 0.4;
+}
+
+function Script16()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="SOUND.mp3";
+audio.load();
+audio.play();
+audio.volume = 0.4;
+}
+
